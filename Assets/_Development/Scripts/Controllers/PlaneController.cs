e40f5a7255dfb01e3bd9e3f3b1db8ab16b5733b8
Intizar <imuhamediyev@gmail.com>
2024-04-18 03:32:32 +0300

    Implemented camera rotation according to plane

--- Assets/_Development/Scripts/Controllers/PlaneController.cs
+++ Assets/_Development/Scripts/Controllers/PlaneController.cs
@@ -25,51 +25,19 @@ namespace PaperDream
         {
             _rigidbody = GetComponent<Rigidbody>();
             _camera = Camera.main;
-
-            _camera.transform.SetParent(null);
         }
 
         private void Update()
         {
             float thrustDelta = 0f;
-            if (Input.GetKey(KeyCode.Space))
-            {
-                thrustDelta += _thrustIncreaseSpeed;
-            }
-
-            if (Input.GetKey(KeyCode.LeftShift))
-            {
-                thrustDelta -= _thrustIncreaseSpeed;
-            }
-
             _currentThrust += thrustDelta * Time.deltaTime;
             _currentThrust = Mathf.Clamp(_currentThrust, _minThrust, _maxThrust);
 
             _deltaPitch = 0f;
-            if (Input.GetKey(KeyCode.S))
-            {
-                _deltaPitch -= _pitchIncreaseSpeed;
-            }
-
-            if (Input.GetKey(KeyCode.W))
-            {
-                _deltaPitch += _pitchIncreaseSpeed;
-            }
-
             _deltaPitch += _pitchIncreaseSpeed * _joystick.Vertical;
             _deltaPitch *= Time.deltaTime;
 
             _deltaRoll = 0f;
-            if (Input.GetKey(KeyCode.A))
-            {
-                _deltaRoll += _rollIncreaseSpeed;
-            }
-
-            if (Input.GetKey(KeyCode.D))
-            {
-                _deltaRoll -= _rollIncreaseSpeed;
-            }
-
             _deltaRoll -= _rollIncreaseSpeed * _joystick.Horizontal;
             _deltaRoll *= Time.deltaTime;
         }
@@ -87,6 +55,10 @@ namespace PaperDream
 
             cameraTransform.position = (cameraTransform.position * _cameraSpring) + (cameraTargetPosition * (1 - _cameraSpring));
             _camera.transform.LookAt(_cameraTarget);
+
+            Vector3 cameraRotation = _camera.transform.rotation.eulerAngles;
+            Quaternion targetRotation = Quaternion.Euler(cameraRotation.x, cameraRotation.y, localRotation.eulerAngles.z);
+            _camera.transform.rotation = targetRotation;
         }
     }
 }
